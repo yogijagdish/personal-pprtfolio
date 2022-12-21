69b203581ec30ab27deef4ce9f50365c775e07a1
yogijagdish <yogijagdish007@gmail.com>
2022-12-21 08:23:52 +0545

    working on nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ export default function Navbar(props) {
   return (
     <nav>
       <ul className="flex flex-row bg-blue-800 py-5">
-        <li className="font-mono text-white basis-2/3 text-center ">Jagdish Yogi</li>
-        <li className="font-mono text-white basis-1/6 text-center"> Home </li>
-        <li className="font-mono text-white basis-1/6 text-center"> About </li>
-        <li className="font-mono text-white basis-1/6 text-center"> Contact Me</li>
+        <li className="font-mono text-white basis-2/3 text-center hover:text-lime-500">Jagdish Yogi</li>
+        <li className="font-mono text-white basis-1/6 text-center hover:text-lime-500"> Home </li>
+        <li className="font-mono text-white basis-1/6 text-center hover:text-lime-500"> About </li>
+        <li className="font-mono text-white basis-1/6 text-center hover:text-lime-500"> Contact Me</li>
       </ul>
     </nav>
   );
