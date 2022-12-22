fe71d18c569c865cf6b7cc2871ccf90c729be2da
yogijagdish <yogijagdish007@gmail.com>
2022-12-22 09:17:39 +0545

    working on bg image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Home from './components/Home';
 import Navbar from './components/Navbar';
 function App() {
   return (
     <>
     <Navbar />
+    <Home/>
     <p> This is tailwind css</p>
     </>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import background from "../images/background.jpg"
+
+export default function Home(props) {
+    return (
+        <>
+        <div style={{backgroundImage: `url(${background})`}}> hello cbia
+        cbasb
+        <br />
+        <br /><br /> <br /><br />
+        <br /><br /> <br /><br />
+        <br /><br /> <br /><br />
+        <br /><br /> <br /><br />
+        <br /><br /> <br /><br /></div>
+        {/* <img src={background} alt="" /> */}
+            </>
+    )
+}
