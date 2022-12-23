d88aad61ebe679904d3835f233043c8ada5838d0
yogijagdish <yogijagdish007@gmail.com>
2022-12-23 08:47:36 +0545

    images added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,19 @@
 import React from "react";
 import background from "../images/background.jpg"
+import image from "../images/images.jpeg"
 
 export default function Home(props) {
     return (
         <>
-        <div style={{backgroundImage: `url(${background})`}}> hello cbia
-        cbasb
-        <br />
-        <br /><br /> <br /><br />
-        <br /><br /> <br /><br />
-        <br /><br /> <br /><br />
-        <br /><br /> <br /><br />
-        <br /><br /> <br /><br /></div>
-        {/* <img src={background} alt="" /> */}
+        <div style={{backgroundImage: `url(${image})`}}>
+            Hi <br />
+             It's Jagdish Yogi.
+             <br/>
+            Student of Electronics, Communication and Engineering.
+            <br/>
+            This is Frontend developer, Backend developer and blockchain Engineer.
+             <img src={background} alt="myimage"/>
+        </div>
             </>
     )
 }
