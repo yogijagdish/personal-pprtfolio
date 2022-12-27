862181a7ed99192d458b4f829f83d5698fc349f2
yogijagdish <yogijagdish007@gmail.com>
2022-12-27 08:37:26 +0545

    flex done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,26 @@
 import React from "react";
-import background from "../images/background.jpg"
-import image from "../images/images.jpeg"
+import background from "../images/background.jpg";
+import image from "../images/images.jpeg";
 
 export default function Home(props) {
-    return (
-        <>
-        <div style={{backgroundImage: `url(${image})`}}>
-            Hi <br />
-             It's Jagdish Yogi.
-             <br/>
-            Student of Electronics, Communication and Engineering.
-            <br/>
-            This is Frontend developer, Backend developer and blockchain Engineer.
-             <img src={background} alt="myimage"/>
+  return (
+    <>
+      <div
+        className="flex flex-row bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${image})` }}
+      >
+        <div>
+          Hi <br />
+          It's Jagdish Yogi.
+          <br />
+          Student of Electronics, Communication and Engineering.
+          <br />
+          This is Frontend developer, Backend developer and blockchain Engineer.
         </div>
-            </>
-    )
+        <div className="flex-initial w-64">
+          <img src={background} alt="myimage" className="float-right" />
+        </div>
+      </div>
+    </>
+  );
 }
