4ff368f0de367db94f7d70aa9eaa2a1618b8bcbc
yogijagdish <yogijagdish007@gmail.com>
2023-06-14 23:57:31 +0545

    code changed by muku

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { AiFillGithub, AiOutlineTwitter, AiFillLinkedin } from "react-icons/ai";
 import { jd } from "../images/images";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 const buttonVariant = {
   hidden: {
@@ -10,8 +10,27 @@ const buttonVariant = {
   show: {
     originX: 0,
     scale: 1.2,
-    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'
-  }
+    textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+  },
+};
+
+const nameVariant = {
+  hidden: {
+    fontSize: "50%",
+    color: "white",
+  },
+  show: {
+    fontSize: "200%",
+    color: "orange",
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+  hover: {
+    scale: 1.3,
+    textShadow: "4px 4px 8px rgba(0, 0, 0, 0.8)",
+  },
 };
 
 const imageVariant = {
@@ -24,8 +43,8 @@ const imageVariant = {
     filter: "brightness(0.6)",
     transition: {
       duration: 0.3,
-    }
-  }
+    },
+  },
 };
 
 export default function Home() {
@@ -35,50 +54,38 @@ export default function Home() {
         <div className="hero-info pb-5 md:pb-0">
           <h1 className="text-4xl lg:text-6xl font-bodyFont">
             Hi, <br />
-            I am <span className="font-extrabold">
+            I am{" "}
+            <span className="font-extrabold">
               <motion.span
-                variants={buttonVariant}
+                variants={nameVariant}
                 initial="hidden"
-                whileHover="show"
+                whileHover="hover"
               >
                 Jagdish Yogi
               </motion.span>
-            </span> <br />
+            </span>{" "}
+            <br />
             Web Developer
           </h1>
-          <motion.div className="pt-8"
-            variants={buttonVariant}
-            initial="hidden"
-            whileHover="show"
-          >
+          <motion.div className="pt-8" variants={buttonVariant} initial="hidden" whileHover="show">
             <button className="font-bodyFont font-bold text-xl h-10 w-52 bg-navColour rounded-lg hover:text-black">
-              <a href="../images/Resume/jagdishcv.pdf" download="jadishcv.pdf">Download Resume</a>
+              <a href="../images/Resume/jagdishcv.pdf" download="jadishcv.pdf">
+                Download Resume
+              </a>
             </button>
           </motion.div>
           <div className="flex py-8">
-            <motion.div
-              variants={buttonVariant}
-              initial="hidden"
-              whileHover="show"
-            >
+            <motion.div variants={buttonVariant} initial="hidden" whileHover="show">
               <a href="https://github.com/yogijagdish" className="pr-4 inline-block hover:text-black">
                 <AiFillGithub size={36} />
               </a>
             </motion.div>
-            <motion.div
-              variants={buttonVariant}
-              initial="hidden"
-              whileHover="show"
-            >
+            <motion.div variants={buttonVariant} initial="hidden" whileHover="show">
               <a href="https://twitter.com/JagdishYogi14" className="pr-4 inline-block hover:text-black">
                 <AiOutlineTwitter size={36} />
               </a>
             </motion.div>
-            <motion.div
-              variants={buttonVariant}
-              initial="hidden"
-              whileHover="show"
-            >
+            <motion.div variants={buttonVariant} initial="hidden" whileHover="show">
               <a href="https://www.linkedin.com/in/jagdishyogi/" className="pr-4 inline-block hover:text-black">
                 <AiFillLinkedin size={36} />
               </a>
@@ -86,13 +93,9 @@ export default function Home() {
           </div>
         </div>
         <div>
-          <motion.img src={jd} alt="gif images"
-            variants={imageVariant}
-            initial="hidden"
-            whileHover="show"
-          />
+          <motion.img src={jd} alt="gif images" variants={imageVariant} initial="hidden" whileHover="show" />
         </div>
       </div>
     </section>
-  )
+  );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,74 +1,133 @@
-// CSS Style for replacing more css
-import "../customCSS/desktopNavbar.css"
-
+import "../customCSS/desktopNavbar.css";
 import React from "react";
-import {logo} from "../images/images"
-
-
-// import from react icons for making icons
-import {BsThreeDots,BsThreeDotsVertical} from "react-icons/bs"
-
+import { logo } from "../images/images";
+import { BsThreeDots, BsThreeDotsVertical } from "react-icons/bs";
 import { useState } from "react";
+import { motion } from "framer-motion";
+
+const buttonVariant = {
+  hidden: {
+    scale: 1,
+    boxShadow: "none",
+  },
+  show: {
+    scale: 1.1,
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.3)",
+    transition: {
+      duration: 0.3,
+    },
+  },
+};
+
+const imageVariant = {
+  initial: {
+    scale: 1,
+    y: 0,
+    boxShadow: "none",
+  },
+  animate: {
+    y: [-10, 0],
+    scale: [1.1, 1],
+    boxShadow: [
+      "0 2px 4px rgba(0, 0, 0, 0.3)",
+      "none",
+    ],
+    transition: {
+      duration: 0.6,
+      repeat: Infinity,
+      repeatType: "mirror",
+    },
+  },
+};
 
 export default function Navbar() {
-
-  // toggle for mobile to display navbar or not
   const [toggle, setToggle] = useState(false);
 
-  // handles when toggle is clicked
   const handleToggle = () => {
     setToggle(!toggle);
     console.log(toggle);
-  }
+  };
 
   return (
-
-    /*  headers is the top of website 
-        flex -> displays the content in flex and maintain their flexibility behaviour
-        justify-between -> maintains the equal spacing between the content of the flex
-        fixed -> makes the header fix and doesnot move durinf scrolling
-        w-full -> has the full width of the screen
-    */
-    <header className="flex justify-between px-5 py-4 bg-teal-500 text-navTextColour fixed w-full z-10">
-
-      <a href="/" className="logo text-2xl font-bodyFont font-bold"> <img src={logo} alt="logo of j" className="float-left h-8 mr-2 ml-2"/> Jagdish </a>
+    <header className="flex justify-between px-5 py-4 bg-gradient-to-r from-teal-500 to-teal-700 text-navTextColour fixed w-full z-10">
+      <a href="/" className="logo text-2xl font-bodyFont font-bold">
+        <motion.img
+          src={logo}
+          alt="logo of j"
+          className="float-left h-8 ml-2"
+          variants={imageVariant}
+          initial="initial"
+          animate="animate"
+        />
+        agdish
+      </a>
 
       {/* desktop navbar */}
-
-      {/* hidden -> hides the navbar for all the screen that has resolution lower than md
-          md:block -> displays all the content in the block */}
-
       <nav className="hidden md:block">
         <ul className="flex font-titleFont font-extrabold">
-          <li> <a href="/#home" > Home </a></li>
-          <li> <a href="/#education"> Education </a> </li>
-          <li> <a href="/#projects"> Projects </a> </li>
-          <li> <a href="/#skills"> Skills </a> </li>
-          <li> <a href="/#contact"> Contact</a> </li>
+          <motion.li
+            className="nav-link"
+            variants={buttonVariant}
+            whileHover="show"
+          >
+            <a href="/#home">Home</a>
+          </motion.li>
+          <motion.li
+            className="nav-link"
+            variants={buttonVariant}
+            whileHover="show"
+          >
+            <a href="/#education">Education</a>
+          </motion.li>
+          <motion.li
+            className="nav-link"
+            variants={buttonVariant}
+            whileHover="show"
+          >
+            <a href="/#projects">Projects</a>
+          </motion.li>
+          <motion.li
+            className="nav-link"
+            variants={buttonVariant}
+            whileHover="show"
+          >
+            <a href="/#skills">Skills</a>
+          </motion.li>
+          <motion.li
+            className="nav-link"
+            variants={buttonVariant}
+            whileHover="show"
+          >
+            <a href="/#contact">Contact</a>
+          </motion.li>
         </ul>
       </nav>
 
       {/* mobile navbar */}
-
-      {/* mobile-nav is css class made seprately in another imported folder
-          left-[-100%] -> pushes the screen to -100% i.e. it is out of the screen
-          left-0 -> displays the content from the extreme left */}
-      <nav className={ !toggle ? "mobile-nav left-[-100%]" : "mobile-nav left-0"}>
-
+      <nav className={!toggle ? "mobile-nav left-[-100%]" : "mobile-nav left-0"}>
         <ul className="flex flex-col">
-          <li> <a href="/#home"> Home </a> </li>
-          <li> <a href="/#education"> Education </a> </li>
-          <li> <a href="/#projects"> Projects </a> </li>
-          <li> <a href="/#skills"> Skills </a> </li>
-          <li> <a href="/#contact"> Contact </a> </li>
+          <li>
+            <a href="/#home">Home</a>
+          </li>
+          <li>
+            <a href="/#education">Education</a>
+          </li>
+          <li>
+            <a href="/#projects">Projects</a>
+          </li>
+          <li>
+            <a href="/#skills">Skills</a>
+          </li>
+          <li>
+            <a href="/#contact">Contact</a>
+          </li>
         </ul>
       </nav>
 
       {/* toggle button */}
       <button onClick={handleToggle} className="block md:hidden">
-        {!toggle ? <BsThreeDots size={30} />: <BsThreeDotsVertical size={30}/>}
+        {!toggle ? <BsThreeDots size={30} /> : <BsThreeDotsVertical size={30} />}
       </button>
-
     </header>
-  )
+  );
 }
