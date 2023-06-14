59cfe72d9c3cb649637da4344962f3eb20e65bd0
yogijagdish <yogijagdish007@gmail.com>
2023-06-14 15:18:52 +0545

    aos animation  added on project and education

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,42 +1,27 @@
-import React, {useEffect,useRef} from "react";
+import React, {useEffect} from "react";
 
 import projects from "../customCSS/projects.js";
 
-import {motion,useInView,useAnimation, delay} from "framer-motion"
+import AOS from "aos";
+
+import "aos/dist/aos.css";
+
 
 
 export default function Projects() {
 
-    const ref = useRef(null);
-
-    const isInView = useInView(ref);
-
-    const animation = useAnimation();
-
-
-
     useEffect(()=>{
-        if (isInView){
-            animation.start({
-                y:0,
-                opacity:1,
-                transition:
-                {
-                duration:1,
-                delay:0.25
-                }
-            })
-        }
-    },[isInView])
-
+        AOS.init();
+        AOS.refresh();
+    },[]);
 
     return(
         <section className="bg-gray-900 text-white p-8 md:p-32 scroll-smooth" id="projects">
             <div className="container md:mx-auto grid">
                 <div className="information-about-projects flex flex-col">
-                    <motion.h1 ref={ref} className="font-titleFont font-extrabold text-center text-2xl md:text-6xl mx-auto border-b-[5px] border-indigo-600 mb-8"
-                    initial={{y:50,opacity:0}}
-                    animate={animation}> Projects </motion.h1>
+                    <h1 className="font-titleFont font-extrabold text-center text-2xl md:text-6xl mx-auto py-6 border-b-[5px] border-indigo-600"
+                     data-aos="fade-up"
+                     data-aos-duration="1000"> Projects </h1>
                     
                     <p className="font-bodyFont text-xl mt-4"> Here are links of some of my projects I have done during my leisure time and college time </p>
                 </div>
@@ -45,7 +30,9 @@ export default function Projects() {
                     {projects.map((projects,i) => {
                         return (
                             <div className="relative " key={i}>
-                                <img src={projects.img} alt={projects.title} className="h-64 w-96 mt-16"/>
+                                <img src={projects.img} alt={projects.title} className="h-64 w-96 mt-16" 
+                                data-aos="zoom-in"
+                                data-aos-duration="1000"/>
                                 <div className="font-titleFont font-extrabold text-2xl mt-4">{projects.title}</div>
                                 {/* this is the content that is displayed on hover */}
                                 <div className="flex mt-16 absolute left-14 top-12 w-64 h-40 bg-gray-600 opacity-0 duration-500 justify-center flex-col hover:opacity-100"  >
