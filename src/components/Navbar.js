490502205accb5ca010e9950bdb99b16092ec44c
yogijagdish <yogijagdish007@gmail.com>
2023-06-16 10:02:08 +0545

    navbar corrected

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,11 +40,11 @@ export default function Navbar() {
 
       <nav className="hidden md:block">
         <ul className="flex font-titleFont font-extrabold">
-          <li> <a href="/#home" > Home </a></li>
-          <li> <a href="/#about"> About </a> </li>
-          <li> <a href="/#projects"> Projects </a> </li>
-          <li> <a href="/#skills"> Skills </a> </li>
-          <li> <a href="/#contact"> Contact</a> </li>
+          <li> <a href="/personal-pprtfolio/#home" > Home </a></li>
+          <li> <a href="/personal-pprtfolio/#about"> About </a> </li>
+          <li> <a href="/personal-pprtfolio/#projects"> Projects </a> </li>
+          <li> <a href="/personal-pprtfolio/#skills"> Skills </a> </li>
+          <li> <a href="/personal-pprtfolio/#contact"> Contact</a> </li>
         </ul>
       </nav>
 
