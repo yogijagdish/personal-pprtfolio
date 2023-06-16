891ea459210254b8fc85f6a32f62cb27c540435b
yogijagdish <yogijagdish007@gmail.com>
2023-06-16 10:13:38 +0545

    navbar error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -56,11 +56,11 @@ export default function Navbar() {
       <nav className={ !toggle ? "mobile-nav left-[-100%]" : "mobile-nav left-0"}>
 
         <ul className="flex flex-col">
-          <li> <a href="/#home"> Home </a> </li>
-          <li> <a href="/#about"> About </a> </li>
-          <li> <a href="/#projects"> Projects </a> </li>
-          <li> <a href="/#skills"> Skills </a> </li>
-          <li> <a href="/#contact"> Contact </a> </li>
+          <li> <a href="/personal-pprtfolio/#home"> Home </a> </li>
+          <li> <a href="/personal-pprtfolio/#about"> About </a> </li>
+          <li> <a href="/personal-pprtfolio/#projects"> Projects </a> </li>
+          <li> <a href="/personal-pprtfolio/#skills"> Skills </a> </li>
+          <li> <a href="/personal-pprtfolio/#contact"> Contact </a> </li>
         </ul>
       </nav>
 
